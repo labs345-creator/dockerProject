be26a3512574908f6f5d724b22a10b0ce9809694
Labone <labs345@gmail.com>
2026-07-03 12:18:23 +0530

    modified docker class

--- src/test/java/docker/setupDocker.java
+++ src/test/java/docker/setupDocker.java
@@ -2,24 +2,34 @@ package docker;
 
 import java.io.IOException;
 
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class setupDocker {
-	@BeforeTest
-	void startDocekerGrid() throws IOException, InterruptedException {
-		Runtime.getRuntime().exec("cmd /c start startDocker_grid.bat");
-		Thread.sleep(15000);
-	}
-	
-	@AfterTest
-	void stopDockerFrid() throws IOException, InterruptedException {
-		
-		Runtime.getRuntime().exec("cmd /c start stopDocker_grid.bat");
-		Thread.sleep(15000);
-			
-		Runtime.getRuntime().exec("taskkill /f /im cmd.exe");
-	}
 
+    @BeforeSuite
+    public void startDockerGrid() throws IOException, InterruptedException {
+
+        System.out.println("Starting Docker Grid...");
+
+        Process process = Runtime.getRuntime().exec("cmd /c startDocker_grid.bat");
+        process.waitFor(); // Wait for the batch file to finish
+
+        Thread.sleep(15000); // Give Docker Grid time to start
+
+        System.out.println("Docker Grid Started.");
+    }
+
+    @AfterSuite
+    public void stopDockerGrid() throws IOException, InterruptedException {
+
+        System.out.println("Stopping Docker Grid...");
+
+        Process process = Runtime.getRuntime().exec("cmd /c stopDocker_grid.bat");
+        process.waitFor(); // Wait for the batch file to finish
+
+        Thread.sleep(10000);
+
+        System.out.println("Docker Grid Stopped.");
+    }
 }
